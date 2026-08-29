4f7b4431158fc6964cc4976926575582ad6edfff
André M. Calhegas Morais <121771985+amcmorais@users.noreply.github.com>
2026-08-29 00:00:53 +0000

    lab: export metabolism_opt helpers from subsistence package

--- src/subsistence/__init__.py
+++ src/subsistence/__init__.py
@@ -4,6 +4,7 @@ from .meter import ResourceMeter, ResourceVector
 from .ledger import SubsistenceLedger, AgentAccount
 from .policy import SubsistencePolicy, PressureAction
 from .runtime import SubsistenceRuntime
+from .metabolism_opt import opted_in, remaining_for, verdict_to_action, gate_spend
 
 __all__ = [
     "ResourceMeter",
@@ -13,4 +14,8 @@ __all__ = [
     "SubsistencePolicy",
     "PressureAction",
     "SubsistenceRuntime",
+    "opted_in",
+    "remaining_for",
+    "verdict_to_action",
+    "gate_spend",
 ]
